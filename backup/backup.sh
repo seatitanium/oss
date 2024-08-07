55c9a970bdd52c78779f666b0e679f1811ae7bad
Subilan <suyuanliang1995@gmail.com>
2024-08-07 16:23:15 +0800

    fix

--- backup/backup.sh
+++ backup/backup.sh
@@ -4,5 +4,5 @@ cd "$(dirname "$0")"
 backup_remote_dir=$(sed '/^backup_remote_dir=/!d;s/.*='// ../oss.ini)
 backup_local_dir=$(sed '/^backup_local_dir=/!d;s/.*='// ../oss.ini)
 time=$(date "+%Y-%m-%d_%H:%M:%S")
-./purge-outdated-backup
-../utils/ossul $backup_local_dir $backup_remote_dir/$time
+./purge-outdated-backup.sh
+../utils/osscp $backup_local_dir $backup_remote_dir/$time
